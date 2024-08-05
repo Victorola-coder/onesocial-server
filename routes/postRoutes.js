de28fe82fd978398cebeeb390e61394c9d21e029
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-05 16:32:00 +0100

    feat: implemented Post Routes

---
+++ routes/postRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { createPost } = require('../controllers/postController');
+const router = express.Router();
+
+router.post('/', createPost);
+
+module.exports = router;
