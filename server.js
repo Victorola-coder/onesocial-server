157c0b7b2f1d1be23eaf62621e3c0334b167a5c7
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-08-05 16:33:05 +0100

    feat: new redirection in the server.js

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 // Routes (we'll add these later)
 app.use('/api/auth', require('./routes/twitterAuthRoutes'));
+app.use('/api/posts', require('./routes/postRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
